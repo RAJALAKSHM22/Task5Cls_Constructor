a95c550e326566ca6b3a40c0f63f1e64e56419b6
RAJALAKSHM22 <rajalakshmiasokan500@gmail.com>
2023-09-16 10:34:46 +0530

    changes updating

--- movie.js
+++ movie.js
@@ -1,1 +1,28 @@
 console.log(" OOPs-CLASS:MOVIE ");
+
+class Movie{
+    constructor(title,studio,rating="PG"){
+        this.title=title;
+        this.studio=studio;
+        this.rating=rating;
+        
+    }
+}
+
+let firstDay= new Movie("A DAY","Film Line","R")
+
+console.log("a)",firstDay);
+
+let secondDay= new Movie("A DAY","Film Line")
+
+console.log( "b)",secondDay);
+
+let thirdDay= new Movie("")
+
+console.log("c)",thirdDay);
+
+
+let fourthDay= new Movie("Casino Royale","Eon Productions","PG 13")
+
+console.log("d)", fourthDay);
+
